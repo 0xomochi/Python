2f6d09d54da57f6fa6aa21d56519a4eef2043ddb
Yumeka Kajiwara <yume@sfc.wide.ad.jp>
2018-05-25 00:17:43 +0900

    not yet

--- Python100/02.py
+++ Python100/02.py
@@ -1,14 +1,10 @@
 # 02.py
 # "yume" + "tamu" の文字を先頭から交互に連結して文字列"ytuammeu"を得よ.
 
-str1 = "yume"
-str2 = "tamu"
-print(str1[::] + str2[::1])
-
-
-'''
-sum = ""
+str1 = 'yume'
+str2 = 'tamu'
+sum = ''
 for c1, c2 in zip(str1, str2):
-sum += c1 + c2
-print(sum)
-'''
+  sum += c1 + c2                        # "a += b" means "a = a + b"
+  print(sum)
+
