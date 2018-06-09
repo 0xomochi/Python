2db8c7a89f5f14e50806bf67e2ba1ed1042aca40
Yumeka Kajiwara <yume@sfc.wide.ad.jp>
2018-06-09 21:34:58 +0900

    0609

--- zip_func.py
+++ zip_func.py
@@ -4,7 +4,7 @@
 print("name / shinchoku / tamupoint")
 
 names = ['yume', 'tamu', 'mochi']
-shinchokus = [500, 600, 1000]
+shinchokus = [800, 600, 1000]
 tamupoints = [400, 1000, 800]
 
 for name, shinchoku, tamupoint in zip(names, shinchokus, tamupoints):
